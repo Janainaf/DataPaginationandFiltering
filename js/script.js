20b2e8a7264a3ca105816c83d197fa218cc996a6
Janainaf <figueira.janaina@gmail.com>
2021-07-05 12:21:34 +0200

    pagination is working, check code with instructions

--- js/script.js
+++ js/script.js
@@ -13,14 +13,14 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-function showPage(list) {
-  // var starIndex = page * 9 - 9;
-  // var endIndex = page * 9;
+function showPage(list, page) {
+  var starIndex = page * 9 - 9;
+  var endIndex = page * 9;
   var studentList = document.querySelector(".student-list").innerHTML;
-  // studentList.innerHTML = "";
+  studentList.innerHTML = "";
 
-  for (var i = 0; i < list.length; i++) {
-    for (; i < 9; i++) {
+  for (var i = starIndex; i < list.length; i++) {
+    for (; i < endIndex; i++) {
       studentList += `<li class="student-item cf">
     <div class="student-details"><img class="avatar" src="${list[i].picture.medium}"alt="Profile Picture">
     <h3> ${list[i].name.first} </h3>
@@ -28,12 +28,11 @@ function showPage(list) {
     </div><div class="joined-details">
     <span class="date"> ${list[i].registered.date}</span></div></li>`;
     }
-    document.querySelector(".student-list").innerHTML = studentList;
-    // studentList.innerHTML = "";
   }
+  document.querySelector(".student-list").innerHTML = studentList;
 }
 
-showPage(data);
+showPage(data, 1);
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
